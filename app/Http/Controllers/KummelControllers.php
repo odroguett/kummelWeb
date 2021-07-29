87220409322930ae64709e3cd15198fa3a9fe59e
odroguett <odroguett@gmail.com>
2021-07-28 23:33:02 -0400

    Avances migracion

--- app/Http/Controllers/KummelControllers.php
+++ app/Http/Controllers/KummelControllers.php
@@ -4,16 +4,17 @@
 namespace App\Http\Controllers;
 
 use App\Negocio\Fabricas\Interfaces\IFabricaProductos;
-
-
+use App\Negocio\Interfaces\IVentas;
 
 class KummelControllers extends Controller
 {
   private $oFabricaProductos;
+  private $oVentas;
 
- public function __construct(IFabricaProductos  $_oFabricaProductos)
+ public function __construct(IFabricaProductos  $_oFabricaProductos,IVentas $_oVentas)
  {
   $this->oFabricaProductos = $_oFabricaProductos;
+  $this->oVentas =$_oVentas;
  }
 
     public function home()
@@ -21,6 +22,7 @@ class KummelControllers extends Controller
 
  //dd($this->oFabricaProductos->ProductosVenta()->obtenerProductosDestacados());
 
-     return view('kummel.home',['productosDestacados' =>  $this->oFabricaProductos->ProductosVenta()->obtenerProductosDestacados()]);
+     return view('kummel.home',['productosDestacados' =>  $this->oFabricaProductos->ProductosVenta()->obtenerProductosDestacados()],
+               ['topVentas' =>  $this->oVentas->obtieneTopVentas()]);
     }
 }
