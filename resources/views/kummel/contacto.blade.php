d4cc193ac6b1a8df805e7572160947ee0cc9a64b
odroguett <odroguett@gmail.com>
2021-08-28 02:29:45 -0400

    Correcciones formulario de contacto.

--- resources/views/kummel/contacto.blade.php
+++ resources/views/kummel/contacto.blade.php
@@ -10,6 +10,7 @@
    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
    <meta name="description" content="">
    <meta name="author" content="">
+   <meta name="csrf-token" content="{{ csrf_token() }}" />
    <link rel="icon" type="image/png" href="img/logo.png">
    <title>Contacto</title>
    <!-- Slick Slider -->
@@ -28,32 +29,24 @@
 
 <body class="fixed-bottom-padding bg-light">
 
-
-   <section class="py-4 osahan-main-body">
-      <div class="container" style="max-width: 60%; ">
-         <div class="row ">
-         <img src="../img/Kummel.jpg"  class="img-contacto  bg-Kumel" alt="Responsive image">
-         </div>
-      </div>
-   </section>
-   
    <section class="py-4 osahan-main-body">
       <div class="container ">
          <div class="row">
 
-            <div class="col-lg-12 p-4 bg-light rounded shadow-sm">
+            <div class="col-lg-12  bg-light rounded border shadow-sm">
 
                <div id="edit_profile">
-                  <div class="p-0">
-                     <form action="../Negocio/enviarDatosContacto.php" method="post">
-
+                 
+                     <form action="../Negocio/enviarDatosContacto.php" method="POST">
+                        @csrf
                         <div class="form-group row ">
-                           <div class="col-lg-6">
+                           <div class="col-lg-4">
                               <label class="text-kumel-titulo" for="exampleInputName1">Tu nombre</label>
                               <input type="text" placeholder="Ingresa tu nombre" class="form-control"
                                  id="exampleInputName1" name="nombre" required>
                            </div>
-                           <div class="col-lg-6">
+                           
+                           <div class="col-lg-4">
                               <label class="text-kumel-titulo" for="exampleInputEmail1">Correo</label>
                               <input type="email" placeholder="Correo" class="form-control" id="exampleInputEmail1"
                                  name="mail" aria-describedby="emailHelp" required>
@@ -62,23 +55,25 @@
 
 
                         </div>
+                       
 
-                        <div class="form-group row">
-                           <div class="col-lg-7">
+                        <div class="form-inline ">
+                           <div class="col-md-5">
                               <textarea class="text-kumel-titulo" rows="10" cols="100" id="ContactFormMessage"
                                  name="mensaje" placeholder="Mensaje" required></textarea>
                            </div>
-
-                           <div class="col-lg-4">
-                              <input type="submit" class="btn btn-kumel-1" style="position: absolute;top: 80%;"
-                                 value="enviar">
-                           </div>
-
-
+                         
+                        </div>
+                       <br>
+                        <div class="col-md-9 " style="float: right;">
+                           <input type="submit" class="btn btn-kumel-1" 
+                              value="enviar">
                         </div>
 
+                       
+
                      </form>
-                  </div>
+                 
                </div>
             </div>
          </div>
@@ -92,3 +87,10 @@
 
 </html>
 @endsection
+<script type="text/javascript">
+   $.ajaxSetup({
+       headers: {
+           'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
+       }
+   });
+</script>
